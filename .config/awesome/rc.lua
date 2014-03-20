cabff28a86e9406334a546ff65c0948e75a60ec2
Eric Potash <eric@k2co3.net>
2014-03-20 16:04:09 -0500

    mutt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -163,7 +163,7 @@ end
 mymainmenu = awful.menu({ items = { 
 	{ "&mutt", function() run_or_raise(terminal .. " -name mutt -e mutt", {instance="mutt"}, 1)  end },
 	{ "&sonata", function() run_or_raise("sonata", {class="Sonata"}, 1) end },
-	{ "&rc.lua", function() run_or_raise("gvim .config/awesome/rc.lua", {name = "rc.lua"}, 1) end },
+	{ "&configs", function() run_or_raise("gvim -name configs -p .config/awesome/rc.lua .Xresources .muttrc", {instance = "configs"}, 1) end },
 	{ "&firefox",  function() run_or_raise("iceweasel", {class = "Iceweasel"}) end },
 	{ "&org", function () run_or_raise("gvim -name org -p docs/org/todo.org docs/org/log.org", {instance="org"}, 1) end  }
 }})
